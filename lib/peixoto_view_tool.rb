a081ea171bf2ac435702e0b58c4d8a577fedf360
Roger Peixoto <duarterogerpeixoto@gmail.com>
2021-03-04 09:05:07 -0300

    added changes to gemspec

---
+++ lib/peixoto_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "peixoto_view_tool/version"
+require_relative "peixoto_view_tool/renderer"
+
+module PeixotoViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/peixoto_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PeixotoViewTool
+  VERSION = "0.1.0"
+end
